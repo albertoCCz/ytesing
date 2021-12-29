cfb05affcb87f1627622c91c0265c6b4f35e47fd
albertoCCz <albertocalzada95@gmail.com>
2021-12-29 17:36:35 +0100

    v1.0.4 - delete loggin messages and enhance icon svg

--- ytesing.js
+++ ytesing.js
@@ -1,14 +1,10 @@
-var counter = 0;
-
-function ytesing(counter){
+function ytesing(){
     var elementWithId       = document.getElementById("confirm-button");
     var elementWithIdExists = !!elementWithId;
 
     if (elementWithIdExists) {
-        counter++;
         elementWithId.click();
-        console.log(`Times used: ${counter}`);
     }
 }
 
-setInterval(ytesing, 1000, counter);
+setInterval(ytesing, 500);
